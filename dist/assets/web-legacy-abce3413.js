f0f20ad31ca31aab0832a7c02a9061adf7649bea
DAVID YARANON <dyaranon126@gmail.com>
2023-06-29 16:50:59 -0700

    updated sign in and register buttons on landing page

---
+++ dist/assets/web-legacy-abce3413.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-7932c4cb.js"],(function(e,s){"use strict";var t;return{setters:[e=>{t=e.W}],execute:function(){e("SplashScreenWeb",class extends t{async show(e){}async hide(e){}})}}}));
