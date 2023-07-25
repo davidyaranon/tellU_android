1dc07406aafc0e1a4c118caf17909e77a509367a
DAVID YARANON <dyaranon126@gmail.com>
2023-07-24 21:01:23 -0700

    added handler for deep links

--- src/AppUrlListener.tsx
+++ src/AppUrlListener.tsx
@@ -1,21 +1,27 @@
 import { App, URLOpenListenerEvent } from "@capacitor/app";
 import { useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { useHistory } from "react-router";
+import auth from "./fbConfig";
 
 const AppUrlListener: React.FC<any> = () => {
-  
+
+  const [user] = useAuthState(auth);
   const history = useHistory();
 
-  // useEffect(() => {
-  //   App.addListener('appUrlOpen', (event: URLOpenListenerEvent) => {
-  //     const domain = 'quantum-61b84.firebaseapp.com'
-  //     const slug = event.url.split(domain);
-  //     const path = slug.pop();
-  //     if (path) {
-  //       history.push(path);
-  //     }
-  //   });
-  // }, []);
+  useEffect(() => {
+    if (user) {
+      App.addListener('appUrlOpen', (event: URLOpenListenerEvent) => {
+        const domain = 'quantum-61b84.firebaseapp.com'
+        const slug = event.url.split(domain);
+        const path = slug.pop();
+        if (user && path && (path.includes('post/') || path.includes('about/'))) {
+          console.log('setting path to ' + path);
+          history.push(path);
+        }
+      });
+    }
+  }, [user]);
 
   return null;
 };
